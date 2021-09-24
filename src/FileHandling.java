50fab29daf6f2fa28530ddd80ec9c1cfa4dcfe61
richa <richardckiely@gmail.com>
2021-09-24 13:51:51 +0100

    exercise4 attempted need to ask questions

--- src/FileHandling.java
+++ src/FileHandling.java
@@ -1,15 +1,12 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Iterator;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 public class FileHandling {
     public static void main(String[] args) {
-        String fileName = "C:\\Users\\t00213739\\Desktop\\hamlet.txt";
+        String fileName = "C:\\Users\\richa\\Desktop\\hamlet.txt";
         Set<String> words = new TreeSet<>();
-
+        Map<Set<String>, Integer> favWords = new HashMap<>();
         try {
             Scanner in = new Scanner(new File(fileName));
             int lineNumber = 1;
@@ -23,8 +20,9 @@ public class FileHandling {
                     //System.out.println( word);
                 }
             }
-            System.out.println(words);
-                System.out.println(words.size());
+            favWords.put(words,lineNumber);
+            System.out.println(favWords);
+             //   System.out.println(words.size());
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
