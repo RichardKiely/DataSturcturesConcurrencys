35847d81e6b2fa44b196f7f31e520ff4526e4fe3
t00213739 <richard.kiely@students.ittralee.ie>
2021-09-30 15:58:36 +0100

    week2 test driven development

---
+++ src/CP3LinkedList.java
@@ -0,0 +1,80 @@
+import java.util.NoSuchElementException;
+//singly linked list with reference to first node only 
+
+/**
+A linked list is a sequence of links with efficient
+element insertion and removal. This class 
+contains a subset of the methods of the standard
+java.util.LinkedList class.
+*/
+
+public class CP3LinkedList<E> {
+     private class Node {
+		public E data;
+		public Node next;
+	}
+	private Node first;
+		
+	/** 
+    	Constructs an empty linked list.
+	*/
+	public CP3LinkedList()
+	{  
+		first = null;
+	}
+ 
+	/**
+ 	Adds an element to the front of the linked list.
+ 	@param element the element to add
+	 */
+	public void addFirst(E element)
+	{  
+		Node newLink = new Node();
+		newLink.data = element;
+		newLink.next = first;
+		first = newLink;
+	}
+
+	public E getFirst() {
+		if (first == null)
+			throw new NoSuchElementException();
+		return first.data;
+	}
+
+	public void removeFirst() {
+		if (first == null)
+			throw new NoSuchElementException();
+		first = first.next;
+	}
+	public void print(){
+		Node current = first;
+		while(current != null){
+			System.out.println(current.data);
+			current = current.next;
+		}
+	}
+	public int size(){
+
+		Node temp = first;
+		int size = 0;
+		while(temp != null)
+		{
+			size += 1;
+			temp = temp.next;
+		}
+		return size;
+
+	}
+	public boolean contains(E e){
+
+		Node current = first;
+		boolean found = false;
+		while(current != null && !found){
+			if(current.data == e)
+				found = true;
+			current = current.next;
+		}
+		return true;
+	}
+
+}
---
+++ tests/CP3LinkedListTest.java
@@ -0,0 +1,51 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CP3LinkedListTest {
+    private CP3LinkedList<String> list;
+
+    @BeforeEach //executed before each test
+    void create(){
+        list = new CP3LinkedList<>();
+    }
+
+    @Test
+    void addFirst() {
+    }
+
+    @Test
+    void getFirst() {
+    }
+
+    @Test
+    void removeFirst() {
+    }
+
+    @Test
+    void print() {
+    }
+
+    @Test
+    void size() {
+
+        int size = list.size();
+        assertEquals(0,list.size());
+
+
+        list.addFirst("Sue");
+        assertEquals(1,list.size());
+
+        list.addFirst("Sue");
+        list.addFirst("Bob");
+        assertEquals(3,list.size());
+
+    }
+
+
+    @Test
+    void contains() {
+
+    }
+}
